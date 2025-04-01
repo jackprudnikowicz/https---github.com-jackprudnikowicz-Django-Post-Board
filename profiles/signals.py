2bf4b4b3daa8f00f5d2642b548eef09427ce1a4c
Jack Prudnikowicz <jackprudnikowicz@gmail.com>
2025-04-01 13:46:34 -0400

    Linked the profiles and posts to the django server. Added my user into the profiles tab. Started adding code to the profiles py files. Also added a new file signals.py

---
+++ profiles/signals.py
@@ -0,0 +1,12 @@
+from .models import Profiles
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def post_save_create_rofile(sender, instance, created, *args, **kwargs):
+    print(sender)
+    print(instance)
+    print(created)
+    if created:
+        Profile.objects.create(user=instance)
