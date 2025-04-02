d8aaff6b7303c81d8d618a75a4b912d3037a1bdd
Jack Prudnikowicz <jackprudnikowicz@gmail.com>
2025-04-02 18:58:53 -0400

    Added cripsy forms to our modal. Also had to download bootstrap5 as this version did not work. had to alter the settings.py file to include the crispy form and bootstrap5

--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -37,12 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Our own apps
+    #our own apps
     'posts',
     'profiles',
-    #Third party apps
+    #3rd party apps
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ['bootstrap5']
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
