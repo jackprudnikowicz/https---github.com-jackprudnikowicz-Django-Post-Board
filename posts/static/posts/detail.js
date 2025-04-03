b93b401b5e17e2ca89e1023556e5b5c0c441e5dd
Jack Prudnikowicz <jackprudnikowicz@gmail.com>
2025-04-03 18:04:30 -0400

    The spnner in the detail window will not go away.

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -1,9 +1,24 @@
 console.log('hello')
 
 const backBtn = document.getElementById('back-btn')
+const url = window.location.href + "data/"
+const spinnerBox = document.getElementById('spinner-box')
+
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
 
 backBtn.addEventListener('click', ()=>{
     history.back()
+})
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+        console.log(response)
+        spinnerBox.classList.add('not-visible')
+    },
+    error: function(error){
+        console.log(error)
+    }
 })
