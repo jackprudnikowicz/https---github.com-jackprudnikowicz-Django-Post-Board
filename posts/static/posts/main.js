f2aeb1e17fd00cfa380254d61e924bcd679cae69
Jack Prudnikowicz <jackprudnikowicz@gmail.com>
2025-04-07 17:18:47 -0400

    Took away the dropzone until the post has been added? Not sure why... The close btn after adding a post will reset the modal window "form".

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -16,7 +16,10 @@ const url = window.location.href
 
 
 const alertBox = document.getElementById('alert-box')
-console.log("csrf token:", csrf[0].value)
+
+const dropzone = document.getElementById('my-dropzone')
+const addBtn = document.getElementById('add-btn')
+const closeBtns =[...document.getElementsByClassName('add-modal-close')]
 
 
 
@@ -160,9 +163,9 @@ postForm.addEventListener('submit', e=>{
                     </div>
                 `)
                 likeUnlikePosts()
-                $('#addPostModal').modal('hide')
+                // $('#addPostModal').modal('hide')
                 handleAlerts('success', 'New post added!')
-                postForm.reset()
+                //  postForm.reset()
 
         },
         error: function(error){
@@ -172,5 +175,16 @@ postForm.addEventListener('submit', e=>{
         }
     })
 })
+
+addBtn.addEventListener('click', e=>{
+    dropzone.classList.remove('not-visible')
+})
+
+closeBtns.forEach(btn=> btn.addEventListener('click', ()=>{
+    postForm.reset()
+    if (!dropzone.classList.contains('not-visible')){
+        dropzone.classList.add('not-visible')
+    }
+}))
 
 getData()
